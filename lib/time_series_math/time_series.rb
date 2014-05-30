a609fe9d053fbf4983d11889b3f971808eefd963
Alex Kukushkin <alex@kukushk.in>
2014-05-30 23:07:53 +0300

    Added tests and methods to TimeSeries

--- lib/time_series_math/time_series.rb
+++ lib/time_series_math/time_series.rb
@@ -5,15 +5,73 @@ module TimeSeriesMath
   class TimeSeries
     attr_reader :data
 
-    def initialize(_args = {})
-
+    def initialize(arr_t = nil, arr_v = nil)
+      @data = []
+      push_array(arr_t, arr_v) if arr_t && arr_v
     end
 
+    def size
+      @data.size
+    end
+
+    def keys
+      @data.map { |d| d[0] }
+    end
+
+    def values
+      @data.map { |d| d[1] }
+    end
+
+    def first
+      @data.first
+    end
+
+    def t_first
+      first && first[0]
+    end
+
+    def last
+      @data.last
+    end
+
+    def t_last
+      last && last[0]
+    end
+
+
     def push(t, v)
+      t = t.to_f
+      i = left_index_at(t)
+      if i.nil?
+        @data.unshift([t, v])
+      else
+        @data.insert(i+1, [t, v])
+      end
     end
 
     def push_array(arr_t, arr_v)
+      arr_data = arr_t.zip(arr_v)
+      @data.concat(arr_data)
+      @data.sort_by! { |d| d[0] }
     end
+
+    def left_index_at(t)
+      t = t.to_f
+      return nil if size == 0 || t_first > t
+      return (size - 1) if t_last <= t
+      d = (t - t_first) / (t_last - t_first)
+      i = (d * size.to_f).to_i
+      if @data[i][0] <= t
+        # scan right
+        i += 1 while i+1 < size && t >= @data[i+1][0]
+        i
+      else
+        # scan left
+        i -= 1 while i > 0 && @data[i][0] > t
+        i
+      end
+    end
+
 
   end
 end # module TimeSeriesMath
--- spec/time_series_math/time_series_spec.rb
+++ spec/time_series_math/time_series_spec.rb
@@ -8,5 +8,44 @@ describe TimeSeries do
     it { expect(subject.last).to be nil }
     it { expect(subject.t_first).to be nil }
     it { expect(subject.t_last).to be nil }
+    it { expect(subject.size).to eql 0 }
+    it { expect(subject.keys).to eql [] }
+    it { expect(subject.values).to eql [] }
+    it { expect(subject.left_index_at(1.0)).to be nil }
   end
+
+  let(:arr_t) { [1.0, 2.0, 3.0] }
+  let(:arr_v) { [111, 222, 333] }
+  it { expect { TimeSeries.new(arr_t, arr_v) }.to_not raise_error }
+
+  context 'when initialized with arrays' do
+    subject { TimeSeries.new(arr_t, arr_v) }
+    it { expect(subject.size).to eql 3 }
+    it { expect(subject.first).to eql [1.0, 111] }
+    it { expect(subject.last).to eql [3.0, 333] }
+    it { expect(subject.t_first).to eql 1.0 }
+    it { expect(subject.t_last).to eql 3.0 }
+
+    describe '#left_index_at' do
+      it 'should return nil if t < first element' do
+        expect(subject.left_index_at(-1.0)).to be nil
+      end
+      it 'should return i_k if T(i_k) <= t < T(i_k+1)' do
+        expect(subject.left_index_at(1.0)).to be 0
+        expect(subject.left_index_at(1.5)).to be 0
+      end
+      it 'should return last element index (N) if T(N) <= t' do
+        expect(subject.left_index_at(10.0)).to be 2
+      end
+    end
+  end
+
+  context 'when adding new items' do
+    subject { TimeSeries.new(arr_t, arr_v) }
+    it 'should place new items at correct place' do
+      expect { subject.push(1.5, 123) }.to_not raise_error
+      expect(subject.keys).to eql [1.0, 1.5, 2.0, 3.0]
+    end
+  end
+
 end
